4ad4ae9676f13527e312f654947dcabebf99bf69
John Watts <johnwatts@gmx.com>
2017-03-13 16:04:53 -0500

    Do while exercises part 2 and 3

--- do_while.php
+++ do_while.php
@@ -1,8 +1,28 @@
 <?php
 
-$a = 0;
+//Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a newline.
+
+// $a = 0;
+
+// do {
+// 	echo "$a" . PHP_EOL;
+// 	$a = $a + 2;
+// } while ($a <= 100); 
+
+//Alter your loop to count backwards by 5's from 100 to -10.
+
+// $a = 100;
+
+// do {
+// 	echo "$a" . PHP_EOL;
+// 	$a = $a - 5;
+// } while ($a >= -10); 
+
+//Create a do-while loop that starts at 2, and displays the result $a * $a on each line while $a is less than 1,000,000.
+
+$a = 2;
 
 do {
 	echo "$a" . PHP_EOL;
-	$a = $a + 2;
-} while ($a <= 100); 
+	$a = $a * $a; 
+} while ($a < 1000000);
