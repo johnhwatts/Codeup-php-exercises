62b8244755f97e955808c2713ee5f08075e89a9a
John Watts <johnwatts@gmx.com>
2017-03-15 11:51:19 -0500

    User chosen increments work and defaults to one when there is no user input

--- for.php
+++ for.php
@@ -10,11 +10,19 @@ $end = trim(fgets(STDIN));
 fwrite(STDIN, 'Enter increment: ');
 $increment = trim(fgets(STDIN));
 
-/*initialization*/ 
-//loop with user chosen increment
-for ($i = $start; $i <= $end; $i+=$increment) {
-		echo $i . PHP_EOL;
-	// }
-} 
+// /*initialization*/ 
+// loop with user chosen increment
 
+
+//Default increment to 1 if no input.
+
+if (is_numeric($increment)) {
+		for ($i = $start; $i <= $end; $i+=$increment) {
+		echo $i . PHP_EOL;
+	} 	
+} else if (!is_numeric($increment)) {
+		for ($i = $start; $i <= $end; $i++) {
+		echo $i . PHP_EOL;
+	} 	
+}
 
