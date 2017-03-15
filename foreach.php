e3a3ebec868af79d2e7c9f89b0e2d32129474055
John Watts <johnwatts@gmx.com>
2017-03-15 16:37:51 -0500

    Constructed a loop that iterates through each value and outputs only values with a type that is scalar.

--- foreach.php
+++ foreach.php
@@ -4,17 +4,19 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 	foreach ($things as $thing) {
-		if (is_string($thing)) {
-        		echo "string" . PHP_EOL;
-    		} elseif (is_null($thing)) {
-    			echo "null" . PHP_EOL;
-    		} elseif (is_array($thing)) {
-    			echo "array" . PHP_EOL;
-    		} elseif (is_bool($thing)) {
-    			echo "boolean" . PHP_EOL;
-    		} elseif (is_float($thing)) {
-    			echo "float" . PHP_EOL;
-    		} elseif (is_integer($thing)) {
-    			echo "integer" . PHP_EOL;
-    		}
+		if (is_scalar($thing)) {
+        		echo $thing . PHP_EOL;
+    		} 
+
+    		//   elseif (is_null($thing)) {
+    		// 	echo "null" . PHP_EOL;
+    		// } elseif (is_array($thing)) {
+    		// 	echo "array" . PHP_EOL;
+    		// } elseif (is_bool($thing)) {
+    		// 	echo "boolean" . PHP_EOL;
+    		// } elseif (is_float($thing)) {
+    		// 	echo "float" . PHP_EOL;
+    		// } elseif (is_integer($thing)) {
+    		// 	echo "integer" . PHP_EOL;
+    		// }
 	}
