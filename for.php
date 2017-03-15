4d5c38518c170a56eebed6043634e5e162ac170f
John Watts <johnwatts@gmx.com>
2017-03-15 15:38:57 -0500

    Added check for user input are numbers. Added swap condition if numbers are entered in reverse, e.g. max = min and min =max

--- for.php
+++ for.php
@@ -1,28 +1,42 @@
 <?php
 // input: A pair of numbers: min and max for a range
-fwrite(STDOUT, 'Enter the start of the range: '); 
-$start = trim(fgets(STDIN));
+do {
+	fwrite(STDOUT, 'Enter the start of the range: '); 
+	$start = trim(fgets(STDIN));
+} while(!is_numeric($start));
 
-fwrite(STDIN, 'Enter the end of the range: ');
-$end = trim(fgets(STDIN));
+do {
+	fwrite(STDIN, 'Enter the end of the range: ');
+	$end = trim(fgets(STDIN));
+} while(!is_numeric($end));
 
 // input: User chooses increment
-fwrite(STDIN, 'Enter increment: ');
+do {
+	fwrite(STDIN, 'Enter increment: ');
 $increment = trim(fgets(STDIN));
+} while(!is_numeric($increment));
 
+// if end is less than start, swap values
+if ($end < $start) {
+	$oldEnd = $end;
+	$oldStart = $start;
+	$start = $oldEnd;
+	$end = $oldStart;
+}
 // /*initialization*/ 
-// loop with user chosen increment
-
-
-//Default increment to 1 if no input.
-
 if (is_numeric($increment)) {
+	// loop with user chosen increment
 		for ($i = $start; $i <= $end; $i+=$increment) {
 		echo $i . PHP_EOL;
 	} 	
 } else if (!is_numeric($increment)) {
+	// Default increment to 1 if no input.
 		for ($i = $start; $i <= $end; $i++) {
 		echo $i . PHP_EOL;
 	} 	
+} else {
+	echo "input not valid";
 }
+
+
 
