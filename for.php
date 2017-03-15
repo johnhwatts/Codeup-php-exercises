992213c3ff1078b2accc60a150a4464c6ef4565c
John Watts <johnwatts@gmx.com>
2017-03-15 11:11:43 -0500

    for loop changed, works with user chosen increments

--- for.php
+++ for.php
@@ -1,17 +1,20 @@
 <?php
 // input: A pair of numbers: min and max for a range
-fwrite(STDOUT, 'Enter the start of the range: ');
+fwrite(STDOUT, 'Enter the start of the range: '); 
 $start = trim(fgets(STDIN));
 
 fwrite(STDIN, 'Enter the end of the range: ');
 $end = trim(fgets(STDIN));
 
-/*initialization*/
-for ($i = $start; $i <= $end; $i++) {
-	//process: modulus
-	if ($i % 2 === 0) {
-		//output only for even numbers
-		echo $i, PHP_EOL;
-	}
-}
+// input: User chooses increment
+fwrite(STDIN, 'Enter increment: ');
+$increment = trim(fgets(STDIN));
+
+/*initialization*/ 
+//loop with user chosen increment
+for ($i = $start; $i <= $end; $i+=$increment) {
+		echo $i . PHP_EOL;
+	// }
+} 
+
 
