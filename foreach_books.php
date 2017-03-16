136b8cd9e6bdc609746fa69e58add9ba14c3c0e5
John Watts <johnwatts@gmx.com>
2017-03-16 13:42:34 -0500

    foreach loops exercise, looping through array of books and echoing the name and info about each book, completed

---
+++ foreach_books.php
@@ -0,0 +1,40 @@
+<?php 
+
+$books = array(
+    'The Hobbit' => array(
+        'published' => 1937,
+        'author' => 'J. R. R. Tolkien',
+        'pages' => 310
+    ),
+    'Game of Thrones' => array(
+        'published' => 1996,
+        'author' => 'George R. R. Martin',
+        'pages' => 835
+    ),
+    'The Catcher in the Rye' => array(
+        'published' => 1951,
+        'author' => 'J. D. Salinger',
+        'pages' => 220
+    ),
+    'A Tale of Two Cities' => array(
+        'published' => 1859,
+        'author' => 'Charles Dickens',
+        'pages' => 544
+    )
+);
+
+// Construct a loop that iterates through each book and then each book's keys and values. Have it output the book's title, then list the key value pairs for the data about each book.
+
+foreach ($books as $book => $bookinfo) {
+		if ($bookinfo['published'] >= 1950) {
+			echo $book . PHP_EOL;
+
+		foreach ($bookinfo as $key => $value) {
+			echo $key . ": " . $value . PHP_EOL;
+	    }
+	}
+}
+
+// Update your loop to only show books that were written after 1950.
+
+
