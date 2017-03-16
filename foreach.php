d133255aa244ddb5d6374d9fc8d30a0279ae7ac7
John Watts <johnwatts@gmx.com>
2017-03-16 10:23:09 -0500

    Added an echo for the word Array to match the exercise requirement

--- foreach.php
+++ foreach.php
@@ -5,10 +5,11 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 	foreach ($things as $thing) {
 		if (is_array($thing)) {
+			echo 'Array ( ';
 			foreach ($thing as $value) {
-    			echo $value . ", ";
+    			echo $value, " ";
     		}
-    			echo "" . PHP_EOL; //pushes data following to next line
+    			echo ")" . PHP_EOL; //pushes data following to next
     		} else {
 				echo $thing . PHP_EOL;
 		}
