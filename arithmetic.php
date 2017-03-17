123859a592ff71d4995bd9576a8e1127970da527
John Watts <johnwatts@gmx.com>
2017-03-17 16:18:36 -0500

    Make the error messages show the values of the arguments. Refactor the error messages into their own throwErrorMessage() function, have the other functions use it for error messaging.

--- arithmetic.php
+++ arithmetic.php
@@ -4,42 +4,44 @@ $a = "I'm outside the scope!";
 $b = "I'm outside the scope too!";
 
 function validate($a, $b) {
-	if (!is_numeric($a) || !is_numeric($b)) {
-    	 return "ERROR: Both arguments must be numbers" . PHP_EOL;
+	if (!is_numeric($a)) {
+    	 return "$a is an ERROR: Both arguments must be numbers" . PHP_EOL;
+    } elseif (!is_numeric($b)){
+    	return "$b is an ERROR: Both arguments must be numbers" . PHP_EOL;
     }
 }
 
 function add($a, $b){
-    $message = validate($a, $b);
-    if ($message) {
-        return $message;
+    $throwErrorMessage = validate($a, $b);
+    if ($throwErrorMessage) {
+        return $throwErrorMessage;
     }
     return $a + $b;
        
 }
 
 function subtract($a, $b){
-    $message = validate($a, $b);
-    if ($message) {
-        return $message;
+    $throwErrorMessage = validate($a, $b);
+    if ($throwErrorMessage) {
+        return $throwErrorMessage;
     }
         return $a - $b;
     }
 
 
 function multiply($a, $b){
-    $message = validate($a, $b);
-    if ($message) {
-        return $message;
+    $throwErrorMessage = validate($a, $b);
+    if ($throwErrorMessage) {
+        return $throwErrorMessage;
     }
         return $a * $b;
    
 }
 
 function divide($a, $b){
-	$message = validate($a, $b);
-    if ($message) {
-        return $message;
+	$throwErrorMessage = validate($a, $b);
+    if ($throwErrorMessage) {
+        return $throwErrorMessage;
     } elseif ($b == 0) {
     	return "ERROR: Cannot divide by 0" . PHP_EOL;
     } 
@@ -48,9 +50,9 @@ function divide($a, $b){
 
 
 function modulus($a, $b){
-    $message = validate($a, $b);
-    if ($message) {
-        return $message;
+    $throwErrorMessage = validate($a, $b);
+    if ($throwErrorMessage) {
+        return $throwErrorMessage;
     }
         return $a % $b;
     
