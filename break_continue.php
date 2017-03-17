af81959e93baf63f2092353f2c13d8b1c644ddd7
John Watts <johnwatts@gmx.com>
2017-03-17 10:59:31 -0500

    Created a for loop that shows all even numbers between 1 and 100 using continue.

--- break_continue.php
+++ break_continue.php
@@ -1,1 +1,11 @@
 <?php
+// Create a for loop that shows all even numbers between 1 and 100 using continue.
+
+for ($i = 1; $i <= 100; $i++) {
+    echo $i . PHP_EOL;
+    if (($i % 2) == 0) {
+    	echo "^ that is an even number." . PHP_EOL;
+        continue;
+    }
+    
+}
