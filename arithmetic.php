00b916dad0f7e29f481ec1253595a1a79a599bcf
John Watts <johnwatts@gmx.com>
2017-03-17 16:12:30 -0500

    Validate divide by 0 errors, display error if attempts to divide by 0 are made. Code updated with a validate  function to clean up the code

--- arithmetic.php
+++ arithmetic.php
@@ -3,52 +3,64 @@
 $a = "I'm outside the scope!";
 $b = "I'm outside the scope too!";
 
+function validate($a, $b) {
+	if (!is_numeric($a) || !is_numeric($b)) {
+    	 return "ERROR: Both arguments must be numbers" . PHP_EOL;
+    }
+}
 
 function add($a, $b){
-    if (is_numeric($a) && is_numeric($b)) {
-        return $a + $b;
-    } else {
-        return "ERROR: Both arguments must be numbers" . PHP_EOL;
+    $message = validate($a, $b);
+    if ($message) {
+        return $message;
     }
+    return $a + $b;
+       
 }
 
 function subtract($a, $b){
-    if (is_numeric($a) && is_numeric($b)) {
-        return $a - $b;
-    } else {
-        return "ERROR: Both arguments must be numbers" . PHP_EOL;
+    $message = validate($a, $b);
+    if ($message) {
+        return $message;
     }
-}
+        return $a - $b;
+    }
+
 
 function multiply($a, $b){
-    if (is_numeric($a) && is_numeric($b)) {
-        return $a * $b;
-    } else {
-        return "ERROR: Both arguments must be numbers" . PHP_EOL;
+    $message = validate($a, $b);
+    if ($message) {
+        return $message;
     }
+        return $a * $b;
+   
 }
 
 function divide($a, $b){
-    if (is_numeric($a) && is_numeric($b)) {
-        return $a - $b;
-    } else {
-        return "ERROR: Both arguments must be numbers" . PHP_EOL;
-    }
+	$message = validate($a, $b);
+    if ($message) {
+        return $message;
+    } elseif ($b == 0) {
+    	return "ERROR: Cannot divide by 0" . PHP_EOL;
+    } 
+       return $a / $b;
 }
 
+
 function modulus($a, $b){
-    if (is_numeric($a) && is_numeric($b)) {
-        return $a % $b;
-    } else {
-        return "ERROR: Both arguments must be numbers" . PHP_EOL;
+    $message = validate($a, $b);
+    if ($message) {
+        return $message;
     }
+        return $a % $b;
+    
 }
 	
 
 // Add code to test your functions here
-echo add(10, 2) . PHP_EOL;
+echo add(10, "bob") . PHP_EOL;
 echo subtract(10, 2) . PHP_EOL;
 echo multiply(10, 2) . PHP_EOL;
-echo divide(10, 2) . PHP_EOL;
+echo divide(10, 0) . PHP_EOL;
 echo modulus(10, 2) . PHP_EOL;
 
