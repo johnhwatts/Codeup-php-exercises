e2a43665ec3682defd3e18189bd9877b5d667cbe
John Watts <johnwatts@gmx.com>
2017-03-17 12:22:08 -0500

    Added an if statment to replicate the switch statement

--- switch.php
+++ switch.php
@@ -30,3 +30,19 @@ switch($dayOfWeek) {
        	echo "Today is Sunday" . PHP_EOL;
         break;
 }
+
+if ($dayOfWeek == '1') {
+	echo "Today is Monday" . PHP_EOL;
+} elseif ($dayOfWeek == '2') {
+	echo "Today is Tuesday" . PHP_EOL;
+} elseif ($dayOfWeek == '3') {
+	echo "Today is Wednesday" . PHP_EOL;
+} elseif ($dayOfWeek == '4') {
+	echo "Today is Thursday" . PHP_EOL;
+} elseif ($dayOfWeek == '5') {
+	echo "Today is Friday" . PHP_EOL;
+} elseif ($dayOfWeek == '6') {
+	echo "Today is Saturday" . PHP_EOL;
+} elseif ($dayOfWeek == '7') {
+	echo "Today is Sunday" . PHP_EOL;
+}
