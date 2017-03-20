87e87d974ea187ac605bbb6702b5a71a94828766
John Watts <johnwatts@gmx.com>
2017-03-20 11:47:27 -0500

    Create a function that returns TRUE or FALSE if an array value is found. Search for Tina and Bob in

--- search-arrays.php
+++ search-arrays.php
@@ -2,4 +2,10 @@
 
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
+// Create a function that returns TRUE or FALSE if an array value is found. Search for Tina and Bob in $names
+$result = array_search('Tina', $names);
+var_dump($result);
+
+
+
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
