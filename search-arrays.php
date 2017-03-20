58ef50a33517ed339b513cfbc9c869ac9cc1d069
John Watts <johnwatts@gmx.com>
2017-03-20 14:24:03 -0500

    Exercise completed

--- search-arrays.php
+++ search-arrays.php
@@ -3,26 +3,31 @@
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 // Create a function that returns TRUE or FALSE if an array value is found. Search for Tina and Bob in $names
-function checkArray($name, $names) {
-       if ((array_search($name, $names)) !== false) {
-          echo  "TRUE, $name is in the array." . PHP_EOL;
+function checkArray($array, $name) {
+    if (array_search($name, $array) !== false) {
+        return  TRUE;
     } else {
-    	echo "FALSE, $name is not in the array." . PHP_EOL;
+    	return FALSE;
     }
 }
 
-checkArray('Bob', $names);
+// echo checkArray($names, 'Bob');
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
 // Create a function to compare 2 arrays that returns the number of values in common between the arrays. Use the 2 example arrays and make sure your solution uses array_search().
 
-// function compareArray($name, $compare) {
-// 	   if ((array_search($name, $compare)) !== false) {
-//           echo  "true" . PHP_EOL;
-//     } else {
-//     	echo "false" . PHP_EOL;
-//     }
-// }
 
-// compareArray('Tina', $compare);
+function compareArrays($listOfNames, $arrayToCompare) {
+	$c = 0; //counter variable
+
+	foreach ($listOfNames as $name) {
+		if(checkArray($arrayToCompare, $name)){
+			$c++;
+		}
+	}
+
+	return $c;
+}
+
+echo "Repeated " . compareArrays($names, $compare) . " times" . PHP_EOL;
