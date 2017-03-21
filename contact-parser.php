e9dd6eed690ac46b3b253b83f73f3901d77d22fc
John Watts <johnwatts@gmx.com>
2017-03-21 11:43:56 -0500

    Updated contacts.txt to remove extra line which was creating an extra array. Completed task to output correct array values

--- contact-parser.php
+++ contact-parser.php
@@ -1,11 +1,23 @@
 <?php
 
 function parseContacts($filename)
-{
+
+{ 
     $contacts = array();
+	$handle = fopen($filename, 'r');
+	$contents = fread($handle, filesize($filename));
+	$person = explode("\n", $contents);
+	fclose($handle);
 
-    // todo - read file and parse contacts
-
+	foreach ($person as $contact) {
+		$info = explode("|", $contact);
+		$info[1] = substr($info[1], 0, 3) . "-" . substr($info[1], 3, 3) . "-" . substr($info[1], 6);
+      	$eachContact = [
+        'name' => $info[0],
+        'number' => $info[1]
+      ];
+      $contacts[] = $eachContact;
+    };
     return $contacts;
 }
 
