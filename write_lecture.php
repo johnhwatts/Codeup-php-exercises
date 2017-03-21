fed991273a724d8d50f0b639037cba9ceffc1909
John Watts <johnwatts@gmx.com>
2017-03-21 15:16:29 -0500

    Created logger.php exercise and this includes the 'write' function lecture

---
+++ write_lecture.php
@@ -0,0 +1,23 @@
+<?php 
+
+	$filename = "fruits.txt";
+	$fruits = ['apple', 'strawberry', 'kiwi', 'grape', 'mango'];
+
+	$names = ['Fer', 'Luis', 'Ryan', 'Justin'];
+
+	function write($filename, $array) {
+		$handle = fopen($filename, 'a');
+
+		foreach ($array as $itemInArray) {
+			fwrite($handle, PHP_EOL . $itemInArray);
+		}
+
+		fwrite($handle, "this is the end" . PHP_EOL);
+
+		fclose($handle);
+	}
+
+	write($filename, $fruits);
+	write('instructors.txt', $names);
+
+?>
