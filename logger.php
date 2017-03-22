1c3cf4b0e5d42d8d255ded8b8e8ae5309fea707c
John Watts <johnwatts@gmx.com>
2017-03-22 09:48:53 -0500

    Updated to final version of logger exercise

--- logger.php
+++ logger.php
@@ -1,6 +1,6 @@
 <?php
 
-function logMessage($logLevel, $message) {
+function logMessage($level, $message) {
 	$today = date("Y-m-d H:i:s");
 	$filename = 'log-YYYY-MM-DD.log';
     
@@ -9,5 +9,15 @@ function logMessage($logLevel, $message) {
     fwrite($handle, PHP_EOL . "$today $logLevel $message");
 }
 
-logMessage("INFO", "This is an info message.");
-logMessage("ERROR", "This is an info message.");
+// logMessage("INFO", "This is an info message.");
+// logMessage("ERROR", "This is an info message.");
+
+function logInfo($level, $message) {
+	logMessage("INFO", "This is an info message.") . PHP_EOL;
+}
+function logError($level, $message) {
+	logMessage("ERROR", "This is an error message.") . PHP_EOL;
+}
+
+logInfo();
+logError();
