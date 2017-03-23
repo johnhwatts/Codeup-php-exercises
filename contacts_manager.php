f6dc718c2e760c52b5e826969bd761fc3d652384
John Watts <johnwatts@gmx.com>
2017-03-23 09:35:34 -0500

    Added search function and added a variable  for the switch cases to call

--- contacts_manager.php
+++ contacts_manager.php
@@ -4,27 +4,22 @@
 
 //Main menu
 
-fwrite(STDOUT, "--- Main Menu ---" . PHP_EOL);	
-fwrite(STDOUT, "1. View Contacts." . PHP_EOL);
-fwrite(STDOUT, "2. Add a new contact." . PHP_EOL);
-fwrite(STDOUT, "3. Search a contact by name." . PHP_EOL);
-fwrite(STDOUT, "4. Delete an existing contact." . PHP_EOL);
-fwrite(STDOUT, "5. Exit. Enter an option (1, 2, 3, 4 or 5): " . PHP_EOL);
+
 
 
 //Function to parse the contacts
 
-function parseContacts($filename)
-
+function parseContacts($file)
 { 
     $contacts = array();
     // Open stream
-    $filename = 'contacts.txt';
+    $filename = $file;
 	$handle = fopen($filename, 'r');
-	$contents = trim(fread($handle, filesize($filename)));
+	$contents = fread($handle, filesize($filename));
+	$contents = trim($contents);
 	// Close stream
     fclose($handle);
-    
+
 	$person = explode("\n", $contents);
 	
 	// Explode each array element into an associative array of name and number key values 
@@ -32,8 +27,8 @@ function parseContacts($filename)
 		$info = explode("|", $contact);
 		$info[1] = substr($info[1], 0, 3) . "-" . substr($info[1], 3, 3) . "-" . substr($info[1], 6);
       	$eachContact = [
-	        'name' => $info[0],
-	        'number' => $info[1]
+	        "name" => $info[0],
+	        "number" => $info[1]
       	];
       $contacts[] = $eachContact;
     }
@@ -47,27 +42,77 @@ function parseContacts($filename)
 
 //function to display contacts appropriately
 
-function displayContact ($contacts) {
+function displayContacts ($contactsArray) {
 	echo str_pad('Name', 10);
 	echo ' | ';
-	echo str_pad('Phone Number', 10);
+	echo str_pad('Phone Number', 12);
 	echo "\n";
 	echo "--------------------------" . PHP_EOL;
-	asort($contacts);
-		foreach ($contacts as $key => $contact) {
-		 	echo $contact['name'] . " | " . $contact['number'] ."\n";
+		foreach ($contactsArray as $key => $contact) {
+		 	echo $contact['name'] . " | " . $contact['number'] . PHP_EOL;
 		 }
 } 
 
-var_dump(displayContact(parseContacts('contacts.txt')));
+
 //function to add contact
 
+function addContact($newContactName, $newContactNumber, $filename) {
+	$handle = fopen($filename, 'a');
+	fwrite($handle, $newContactName ." | " . $newContactNumber); 
+	fclose($handle);
+}
+
 //function to search contacts
+function searchContacts($contactsArray, $contactSearch) {
+	$results = [];
+	foreach ($contactsArray as $key => $contact) { 
+    	if (stripos($contact["name"], $contactSearch) !== false) {
+    		array_push($results, $contact);
+        } 
+	} return $results;
+}
+
 
 //function to delete contacts
+function deleteContacts() {
+
+}
+
 
 //Loop to return the functions
 
+function mainMenu($file) {
+	fwrite(STDOUT, "--- Main Menu ---" . PHP_EOL);	
+	fwrite(STDOUT, "1. View Contacts." . PHP_EOL);
+	fwrite(STDOUT, "2. Add a new contact." . PHP_EOL);
+	fwrite(STDOUT, "3. Search a contact by name." . PHP_EOL);
+	fwrite(STDOUT, "4. Delete an existing contact." . PHP_EOL);
+	fwrite(STDOUT, "5. Exit. Enter an option (1, 2, 3, 4 or 5): " . PHP_EOL);
+	$input = trim(fgets(STDIN));
+	$result = parseContacts($file);
+	switch($input) {
+		case 1:
+			displayContacts($result);
+			break;
+		case 2:
+			fwrite(STDOUT, "Enter name for new contact" . PHP_EOL);
+			$newContactName = trim(fgets(STDIN));
+			fwrite(STDOUT, "Enter number for new contact" . PHP_EOL);
+			$newContactNumber = trim(fgets(STDIN));
+			addContact(PHP_EOL . $newContactName, $newContactNumber, $file);
+			break;
+		case 3:
+			fwrite(STDOUT, "Enter name of contact to search" . PHP_EOL);
+			$contactSearch = trim(fgets(STDIN));
+			displayContacts(searchContacts($result, $contactSearch));
+			break;
+		case 4:
+			deleteContact();
+			break;
+	}
+}
+
+mainMenu('contacts.txt');
 
 
 
